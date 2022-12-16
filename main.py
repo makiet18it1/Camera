342aac539079bab300f5a1c810ff72050bed54ac
kiệt mai <you@example.com>
2022-12-16 23:00:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,112 +1,127 @@
-# Import the required libraries
 import numpy as np
-
-import time
-import argparse
-import datetime
 import cv2
-import imutils
+import time
+import datetime
 from collections import deque
+import telegram
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="Nguồn video mặc định 0 là sử dụng webcam", default='0', type=str)
-ap.add_argument("-a", "--min-area", type=int, default=300, help="Kích thước nhỏ nhất của đối tượng")
-ap.add_argument("-b", "--background", type=str, default=None, help="Ảnh background")
-args = vars(ap.parse_args())
 
-cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+def is_person_present(frame, thresh=1100):
+    global foog
+    fgmask = foog.apply(frame)
+    ret, fgmask = cv2.threshold(fgmask, 250, 255, cv2.THRESH_BINARY)
+    fgmask = cv2.dilate(fgmask, None, iterations=4)
+    contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours and cv2.contourArea(max(contours, key=cv2.contourArea)) > thresh:
+        cnt = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(frame, 'Person Detected', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1, cv2.LINE_AA)
 
-start_time = time.time()
+        return True, frame
 
+    else:
+        return False, frame
+
+
+# def send_message(body, info_dict):
+#
+#     account_sid = info_dict['account_sid']
+#     auth_token  = info_dict['auth_token']
+#     client = Client(account_sid, auth_token)
+#     message = client.messages.create( to = info_dict['your_num'], from_ = info_dict['trial_num'], body= body)
+
+def sendMessage(body):
+    my_token = "5722526497:AAEI_FoFddGf55OJZlDtZgtsC3GaJn0SkNM"
+    bot = telegram.Bot(token=my_token)
+    bot.sendMessage(chat_id="5408192472", text=body)
+
+
+cv2.namedWindow('frame', cv2.WINDOW_KEEPRATIO)
+cap = cv2.VideoCapture('http://192.168.1.5:8080/video')
+width = int(cap.get(3))
+height = int(cap.get(4))
+#
+# with open('credentials.txt', 'r') as myfile:
+#   data = myfile.read()
+#
+# info_dict = eval(data)
+
+foog = cv2.createBackgroundSubtractorMOG2(detectShadows=True, varThreshold=100, history=2000)
+
+status = False
+
+patience = 7
+
+detection_thresh = 15
+
+initial_time = None
+
+de = deque([False] * detection_thresh, maxlen=detection_thresh)
+
+# Initialize these variables for calculating FPS
 fps = 0
 frame_counter = 0
-
-if args['video'] == '0':
-    cap = cv2.VideoCapture('http://192.168.1.9:8080/video')
-else:
-    cap = cv2.VideoCapture(args['video'])
-
-first_frame = None
-if args['background'] is not None:
-    first_frame = cv2.imread(args['background'])
-
-    first_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
-
-    first_gray = cv2.GaussianBlur(first_gray, (21, 21), 0)
-
-idx = 0
-
-top_left, bottom_right = (100, 700), (500, 100)
-
-# img_counter = 0
+start_time = time.time()
 
 while (True):
 
     ret, frame = cap.read()
-    if frame is None:
+    if not ret:
         break
-    text = "An toan"
-    # Resize ảnh về kích thước cố định
-    frame = imutils.resize(frame, width=1500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # cv2.imshow("Current frame", frame)
+    detected, annotated_image = is_person_present(frame)
+    de.appendleft(detected)
 
-    if first_frame is None:
-        first_frame = frame
-        first_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
-        first_gray = cv2.GaussianBlur(first_gray, (21, 21), 0)
+    if sum(de) == detection_thresh and not status:
+        status = True
+        entry_time = datetime.datetime.now().strftime("%A, %I-%M-%S %p %d %B %Y")
+        out = cv2.VideoWriter('outputs/{}.mp4'.format(entry_time), cv2.VideoWriter_fourcc(*'XVID'), 15.0,
+                              (width, height))
 
-    frameDelta = cv2.absdiff(first_gray, gray)
+    if status and not detected:
 
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        if sum(de) > (detection_thresh / 2):
+            if initial_time is None:
+                initial_time = time.time()
 
-    thresh = cv2.dilate(thresh, None, iterations=2)
+        elif initial_time is not None:
+            if time.time() - initial_time >= patience:
+                status = False
+                exit_time = datetime.datetime.now().strftime("%A, %I:%M:%S %p %d %B %Y")
+                out.release()
+                initial_time = None
 
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+                body = "Alert: \n A Person Entered the Room at {} \n Left the room at {}".format(entry_time, exit_time)
+                sendMessage(body)
 
-    cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
-    cv2.putText(frame, "Tinh trang: {}".format(text), (00, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    elif status and sum(de) > (detection_thresh / 2):
+        initial_time = None
+
+    # Get the current time in the required format
+    current_time = datetime.datetime.now().strftime("%A, %I:%M:%S %p %d %B %Y")
+
+    cv2.putText(annotated_image, 'FPS: {:.2f}'.format(fps), (1500, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 40, 155),
+                2)
+    cv2.putText(annotated_image, current_time, (310, 20), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 0, 255), 2)
+    cv2.putText(annotated_image, 'Room Occupied: {}'.format(str(status)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                (200, 10, 150), 2)
+
+    if initial_time is None:
+        text = 'Patience: {}'.format(patience)
+    else:
+        text = 'Patience: {:.2f}'.format(max(0, patience - (time.time() - initial_time)))
+
+    cv2.putText(annotated_image, text, (1000, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 40, 155), 2)
+
+    if status:
+        out.write(annotated_image)
+
+    cv2.imshow('frame', frame)
     frame_counter += 1
     fps = (frame_counter / (time.time() - start_time))
-
-    # Display the FPS
-    cv2.putText(frame, 'FPS: {:.2f}'.format(fps), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1)
-
-    for c in cnts:
-
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
-
-        (x, y, w, h) = cv2.boundingRect(c)
-
-        center_x = x + w / 2
-        center_y = y + h / 2
-
-        logic = top_left[0] < center_x < bottom_right[0] and top_left[1] < center_y < bottom_right[1]
-
-        if logic:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            text = "Co xam nhap"
-
-    cv2.imshow('Camera An Ninh', frame)
-    # cv2.imshow("Thresh", thresh)
-    cv2.imwrite('/images/{}_result.png'.format(idx), frame)
-    cv2.imwrite('../images/{}_delta.jpg'.format(idx), frameDelta)
-    # cv2.imshow("Frame Delta", frameDelta)
-    idx += 1
-
-
-
-    # Exit if q is pressed.
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(30) == ord('q'):
         break
 
-# Release Capture and destroy windows
 cap.release()
 cv2.destroyAllWindows()
